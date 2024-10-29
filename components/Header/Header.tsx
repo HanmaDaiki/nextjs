f5f8a19b3ec597f49b5f88697b59931be54c9d82
HanmaDaiki <daikihanma@yandex.ru>
2024-10-29 17:19:41 +0300

    Fix header color and width

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
     };
 
     return (
-        <div className="flex justify-end items-center w-full h-100 sticky top-0 left-0 p-2 gap-2 bg-black">
+        <div className="flex justify-end items-center w-screen dark:bg-[#0a0a0a] bg-[#ffffff] h-100 sticky top-0 left-0 p-2 gap-2">
             <button className="bg-green-500 text-white px-4 py-2 rounded-md">Редактировать</button>
             <button className="bg-red-500 text-white px-4 py-2 rounded-md" onClick={handleLogout}>Выход</button>
         </div>
