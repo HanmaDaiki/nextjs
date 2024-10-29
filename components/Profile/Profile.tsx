86da7b9caecf98a7dcf5278d04c9f40db25c8bc1
HanmaDaiki <daikihanma@yandex.ru>
2024-10-29 16:18:40 +0300

    add svgr and types for svgr

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -11,9 +11,9 @@ export default function Wrapper() {
             <Image src={Ava} priority alt="profile" className="rounded-full h-[150px] w-[150px] object-cover" />
             <div className="flex flex-col gap-1 items-center">
                 <h1 className="text-2xl font-bold flex gap-1 items-center">
-                    {name} {surname}
+                    {name} <span className="text-blue-500">{surname[0].toUpperCase()}.</span>
                     <button>
-                        <Image className="dark:invert" src={Edit} alt='edit' />
+                        <Edit className="w-5 h-5 text-lime-700" />
                     </button>
                 </h1>
                 <span className="text-gray-500 text-xs">{grade} грейд</span>
